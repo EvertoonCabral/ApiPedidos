25e16eac94110a8ee6955f08959fba9c9d0c14a9
Everton Cabral <96452540+EvertoonCabral@users.noreply.github.com>
2024-10-19 01:59:47 -0300

    Criando Camada de dominio
    Feat: Criando camada de dominio, entidades, enums e as interfaces do repository

--- ControlePedidos/ControlePedidos/obj/Debug/net8.0/ControlePedidos.AssemblyInfo.cs
+++ ControlePedidos/ControlePedidos/obj/Debug/net8.0/ControlePedidos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControlePedidos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fac153d1094195f3e4cf60819b789aa70dfdfb6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0679ce15e66631bccec738772058168593eee9f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControlePedidos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControlePedidos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
